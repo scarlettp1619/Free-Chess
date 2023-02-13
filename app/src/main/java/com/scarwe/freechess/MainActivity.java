6ea765146ebb18ba7f59e0ba03d40830d07e9d84
scarlettp1619 <95775118+scarlettp1619@users.noreply.github.com>
2023-02-13 04:39:44 +0000

    implemented networking
    might not be 100% working

--- app/src/main/java/com/scarwe/freechess/MainActivity.java
+++ app/src/main/java/com/scarwe/freechess/MainActivity.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity implements ChessDelegate {
 
-    BoardModel board = new BoardModel();
+    private final BoardModel board = new BoardModel();
+    private final int PORT = 8080;
+    private PrintWriter printWriter;
+
     public static String tag = "MainActivity";
 
     @Override
@@ -23,13 +34,57 @@ public class MainActivity extends AppCompatActivity implements ChessDelegate {
         boardView.chessDelegate = this;
 
         Button resetButton = findViewById(R.id.reset_button);
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                board.resetBoard();
-                boardView.invalidate();
-            }
+        Button listenButton = findViewById(R.id.listen_button);
+        Button connectButton = findViewById(R.id.connect_button);
+
+        resetButton.setOnClickListener(v -> {
+            board.resetBoard();
+            boardView.invalidate();
         });
 
+        listenButton.setOnClickListener(v -> {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Log.d(tag, "socket server listening...");
+            executor.execute(() -> ((Runnable) () -> {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(PORT);
+                    Socket socket = serverSocket.accept();
+                    receiveMove(socket);
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).run());
+        });
+
+        connectButton.setOnClickListener(v -> {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute(() -> ((Runnable) () -> {
+                try {
+                    Log.d(tag, "socket client connecting to addr:port");
+                    Socket socket = new Socket(InetAddress.getByName("localhost"), PORT);
+                    receiveMove(socket);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }).run());
+        });
+
+    }
+
+    private void receiveMove(Socket socket) throws IOException {
+        Scanner scanner = new Scanner(socket.getInputStream());
+        printWriter = new PrintWriter(socket.getOutputStream(), true);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] numberStrs = line.split(",");
+            int[] move = new int[numberStrs.length];
+            for (int i = 0; i < numberStrs.length; i++){
+                move[i] = Integer.parseInt(numberStrs[i]);
+            }
+            runOnUiThread(() -> movePiece(move[0], move[1], move[2], move[3]));
+        }
     }
 
     @Override
@@ -41,5 +96,7 @@ public class MainActivity extends AppCompatActivity implements ChessDelegate {
     public void movePiece(int fromCol, int fromRow, int toCol, int toRow) {
         board.movePiece(fromCol, fromRow, toCol, toRow);
         findViewById(R.id.board_view).invalidate();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> System.out.println(fromCol +" "+fromRow +" "+toCol +" "+toRow));
     }
 }
